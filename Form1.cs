e937a1d4ef4d991cc4fb23eaf793c49bce5f84a3
nena03 <69650515+nena03@users.noreply.github.com>
2021-01-22 10:05:22 +0100

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Kucica
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random r = new Random();
+        int x, y,j,l;
+
+       
+
+        Kucica[] k = new Kucica[20];
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+            y = ClientRectangle.Height/2-500;
+            x= ClientRectangle.Width/2-50;
+            
+            
+             timer1.Start();
+             y += 80;
+             x = r.Next(0, 400);
+
+               
+            
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = CreateGraphics();
+
+
+
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Graphics g = CreateGraphics();
+            g.Clear(BackColor);
+            Kucica k = new Kucica(x,y);
+
+            k.Crtaj(g);
+            if (y < ClientRectangle.Height) 
+            {
+                y += 50;
+
+            }
+            else {Refresh();
+                y = -50;
+               
+                
+            }
+
+        }
+
+    }
+}
---
+++ Kucica.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Kucica
+{
+    class Kucica
+    {
+        protected int x, y;
+        protected int a = 100;
+        public Kucica(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+          
+        }
+        public void Crtaj(Graphics g)
+        {
+            Pen olovka = new Pen(Color.Black,15);
+            SolidBrush cetkav = new SolidBrush(Color.Brown);
+            SolidBrush cetkak = new SolidBrush(Color.Red);
+            SolidBrush cetkaf = new SolidBrush(Color.Lime);
+            Point point1 = new Point(x - a / 2, y + a / 2);
+            Point point2 = new Point(x - a / 6, y + a / 2);
+            Point point3 = new Point(x - a / 6, y);
+            Point point4 = new Point(x + a / 6, y);
+            Point point5 = new Point(x + a / 6, y + a / 2);
+            Point point6 = new Point(x + a / 2, y + a / 2);
+            Point point7 = new Point(x + a / 2, y - a / 2);
+            Point point8 = new Point(x, y - a);
+            Point point9 = new Point(x - a / 2, y - a / 2);
+            Point[] krov = { point7, point8, point9 };
+            Point[] fasada = { point1, point2, point3, point4, point5, point6, point7, point9 };
+            Point[] vrata = { point2, point3, point4, point5 };
+            g.DrawLine(olovka, point1, point6);
+            g.DrawLine(olovka, point6, point7);
+            g.DrawLine(olovka, point7, point8);
+            g.DrawLine(olovka, point8, point9);
+            g.DrawLine(olovka, point9, point1);
+            g.DrawLine(olovka, point3, point4);
+            g.DrawLine(olovka, point2, point3);
+            g.DrawLine(olovka, point5, point4);
+            g.FillPolygon(cetkak, krov);
+            g.FillPolygon(cetkaf, fasada);
+            g.FillPolygon(cetkav, vrata);
+        }
+    }
+}
